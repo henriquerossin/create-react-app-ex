a9cef03f73b3af3c1c1d4711a336ad6f364e9ba4
Henrique Rossin <pro.henriquerossin@gmail.com>
2025-01-09 06:56:52 -0300

    criando a seção de projetos e o componente de projeto

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,6 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
@@ -9,6 +10,10 @@ const Sobre = () => (
       id dignissimos aspernatur laborum assumenda molestiae ea distinctio
       debitis velit hic aut omnis laudantium quae, itaque magni mollitia. Harum?
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=henriquerossin&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=henriquerossin&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
